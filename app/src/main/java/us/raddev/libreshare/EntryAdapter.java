70beda3809cbe28b2b05362d9af0ca538a45e8e4
raddevus <1radeutsch1@gmail.com>
2019-03-01 11:55:19 -0500

    Finally got this highlight text thing working.  Wasted so much time on it so I'm checking it in so I don't lose it. Each item will highlight and then lost highglight when not selected.

--- app/src/main/java/us/raddev/libreshare/EntryAdapter.java
+++ app/src/main/java/us/raddev/libreshare/EntryAdapter.java
@@ -3,7 +3,11 @@ package us.raddev.libreshare;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
+import android.text.SpannableString;
+import android.text.style.BackgroundColorSpan;
+import android.text.style.StyleSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,10 +16,16 @@ import android.widget.TextView;
 
 import java.util.List;
 
+import static android.view.HapticFeedbackConstants.LONG_PRESS;
 import static us.raddev.libreshare.MainActivity.mConfig;
 
 public class EntryAdapter extends RecyclerView.Adapter<EntryAdapter.ViewHolder>{
     public List<Entry> allEntries;
+    static SpannableString spannableStyle;
+    static SpannableString previousStyle;
+    static String previousText;
+    static BackgroundColorSpan textStyle;
+
     public EntryAdapter() {
 
     }
@@ -36,14 +46,38 @@ public class EntryAdapter extends RecyclerView.Adapter<EntryAdapter.ViewHolder>{
                 public boolean onLongClick(View view) {
                     Log.d("MainActivity", "entryTextView LongClick! : "
                             + entryTextView.getText());
-                    entryTextView.setSelected(true);
+                    Log.d("MainActivity", "got focus : " + String.valueOf(view.getId()));
+                    entryTextView.performHapticFeedback(LONG_PRESS);
+
+                    String initialText = entryTextView.getText().toString();
+
+                    spannableStyle = new SpannableString(initialText);
+                    textStyle =  new BackgroundColorSpan(Color.YELLOW);
+
+                    spannableStyle.setSpan(textStyle, 0, spannableStyle.length(), 0);
+                    entryTextView.setText(spannableStyle);
 
                     Log.d("MainActivity", "copying to clipboard");
                     ClipboardManager clipboard = (ClipboardManager)view.getContext().getSystemService(Context.CLIPBOARD_SERVICE);
                     ClipData clip = ClipData.  newPlainText(mConfig.getUserId() ,
                             mConfig.getUserId()+":"+entryTextView.getText());
                     clipboard.setPrimaryClip(clip);
+                    previousStyle = spannableStyle;
                     return true;
+                }
+            });
+
+            entryTextView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if (!hasFocus) {
+
+                        previousText = entryTextView.getText().toString();
+                        Log.d("MainActivity", "lost focus : " + String.valueOf(v.getId()) + " : " + previousText);
+                        previousStyle = new SpannableString(previousText);
+                        previousStyle.removeSpan(previousStyle);
+                        entryTextView.setText(previousStyle);
+                    }
                 }
             });
             //league = (TextView) itemView.findViewById(R.id.tvLeague);
